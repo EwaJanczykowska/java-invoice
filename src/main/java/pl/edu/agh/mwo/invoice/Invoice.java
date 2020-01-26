cab2d83d38fb7037a9ff0551b3d901b7a67e214f
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-01-26 20:29:14 +0100

    4.testInvoiceHasProperSubtotalForManyProducts

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,15 +1,16 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Collection<Product> products = new ArrayList<>();
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.add(product);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
@@ -17,7 +18,11 @@ public class Invoice {
 	}
 
 	public BigDecimal getSubtotal() {
-		return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO;
+		for (Product product: products) {
+			sum = sum.add(product.getPrice());
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
